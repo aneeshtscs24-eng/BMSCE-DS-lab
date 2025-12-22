eaa41a43f92d13e0bfdc6a82b75249a02157244c
aneeshtscs24-eng <aneeshts.cs24@bmsce.ac.in>
2025-12-22 18:15:28 +0530

    Add files via upload

---
+++ Graphs DFS.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+void dfs(int);
+int n,i,j,a[10][10],vis[10],count;
+void main()
+{
+    printf("Enter the number of vertices\n");
+    scanf("%d",&n);
+    printf("Enter the adjacency matrix\n");
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+        vis[i]=0;
+    }
+    printf("dfs traversal\n");
+    for(i=1;i<=n;i++)
+    {
+        if(vis[i]==0)
+        dfs(i);
+    }
+    for (i = 1; i <= n; i++)
+    {
+        if (vis[i])
+        {
+            count++;
+        }
+    }
+    if(count==n)
+        printf("Graph is connected");
+    else
+        printf("Graph is not connected");
+}
+void dfs(int v)
+{
+    vis[v]=1;
+    printf("%d",v);
+    for(j=1;j<=n;j++)
+    {
+        if(a[v][j]==1&&vis[j]==0)
+            dfs(j);
+    }
+}
